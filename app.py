6f15f748a5d43ccc8d0a63a84f9c69cad527e43c
Ali Murteza <alimurteza0550@gmail.com>
2025-02-10 21:18:12 +0330

    created a very simple  game with python

---
+++ app.py
@@ -0,0 +1,11 @@
+secret_num = 9
+guess_counter = 0
+guess_limit = 3
+while guess_counter < guess_limit:
+    guess = int(input("Guess : "))
+    guess_counter += 1
+    if guess == secret_num:
+        print("Congratulations! You guessed it!")
+        break
+else :
+    print("Sorry, You failed")
